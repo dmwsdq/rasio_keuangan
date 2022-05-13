699755ec717c1ddf191adfee8c1d0fb546203762
Darmawan <darmawan.mof.ri@gmail.com>
2022-05-13 22:09:58 +0700

    ok

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import math
 
 st.title('Rasio Keuangan')
 st.write('Setiap wirausahawan perlu mengetahui rasio keuangan yang bisa membantu mereka dalam mengelola bisnis. Aplikasi ini akan membantu anda menghitung dan menjelaskan rasio keuangan berikut untuk mengelola bisnis anda agar semakin maju:')
@@ -8,8 +9,8 @@ with st.expander("Breakeven"):
     total_biaya_per_hari = st.number_input('Total biaya per hari')
     harga_barang_per_satuan = st.number_input('Harga barang per satuan')
     if st.button('Hitung'):
-        breakeven = str(int(total_biaya_per_hari / harga_barang_per_satuan)+1)
-        st.write('Jumlah barang yang harus anda jual tiap hari sebelum anda bisa untung adalah'+breakeven+'buah. Jika anda sulit mencapai jumlah itu, anda harus menaikkan harga barang atau menurunkan biaya usaha supaya bisa untung.')
+        breakeven = str(math.ceil(total_biaya_per_hari / harga_barang_per_satuan))
+        st.write('Jumlah barang yang harus anda jual tiap hari sebelum anda bisa untung adalah '+breakeven+' buah. Jika anda sulit mencapai jumlah itu, anda harus menaikkan harga barang atau menurunkan biaya usaha supaya bisa untung.')
 
 '''with st.expander("Profit Margin"):
     st.write('Breakeven (titik impas) adalah jumlah barang/jasa yang harus Anda jual sebelum Anda bisa mendapatkan keuntungan.')
