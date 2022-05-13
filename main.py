55da9d020d00911eb20ed439594e877ff31d21d7
Darmawan <darmawan.mof.ri@gmail.com>
2022-05-13 23:29:43 +0700

    ok

--- main.py
+++ main.py
@@ -20,4 +20,18 @@ with st.expander("Profit Margin"):
         profit_margin = round((penjualan_kotor - total_biaya_usaha)/ penjualan_kotor,2)
         st.write('Dari setiap 1 rupiah penjualan kotor anda, anda mendapatkan keuntungan sebesar '+ str(profit_margin) + ' rupiah. Dengan kata lain, jika anda berhasil mendapat total penjualan kotor sebesar Rp100.000, keuntungan anda adalah '+ str(profit_margin*100000) + ' rupiah. Jika anda merasa keuntungannya terlalu sedikit, anda harus meningkatkan penjualan anda atau menurunkan biaya usaha anda.')
 
-     
+with st.expander("Sales Growth"):
+    st.write('Pertumbuhan penjualan adalah rasio yang paling tepat untuk mengukur pertumbuhan usaha anda. Pertumbuhan penjualan bisa dihitung per minggu, per bulan, per tahun, atau sesuai kebutuhan anda.')
+    penjualan_periode_sebelumnya  = st.number_input('Penjualan Periode Sebelumnya')
+    penjualan_periode_ini = st.number_input('Penjualan Periode Ini')
+    if st.button('Hitung  '):
+        sales_growth = round((penjualan_periode_sebelumnya - penjualan_periode_ini)/ penjualan_periode_sebelumnya,2)*100
+        st.write('Pertumbuhan penjualan anda adalah '+ str(sales_growth) + '\%\. Dengan kata lain, penjualan anda naik '+ str(sales_growth) + '\% \dari periode sebelumnya. Jika anda merasa pertumbuhannya terlalu kecil, anda harus meningkatkan penjualan anda.')   
+
+with st.expander("Burn Rate"):
+    st.write('Tingkat pembakaran adalah jumlah uang yang anda habiskan di usaha anda selama jangka waktu tertentu.')
+    uang_awal_tahun  = st.number_input('Uang untuk Modal Usaha di Awal Tahun')
+    uang_akhir_tahun  = st.number_input('Uang untuk Modal Usaha di Akhir Tahun ')
+    if st.button('Hitung   '):
+        burn_rate = round((uang_akhir_tahun - uang_awal_tahun)/12,2) 
+        st.write('Tingkat pembakaran anda adalah Rp'+ str(burn_rate) + '/bulan. Dengan kata lain, tiap bulan anda menghabiskan uang untuk usaha anda sebanyak Rp'+ str(burn_rate)+'. Jika anda merasa tingkat pembakarannya terlalu besar, anda harus menekan biaya usaha anda.')   
