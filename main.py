9824f9a4b4dfbd51b7c2015a3ca8de35a9bcef4b
Darmawan <darmawan.mof.ri@gmail.com>
2022-05-13 23:15:06 +0700

    ok

--- main.py
+++ main.py
@@ -9,15 +9,15 @@ with st.expander("Breakeven"):
     total_biaya_per_hari = st.number_input('Total biaya per hari')
     harga_barang_per_satuan = st.number_input('Harga barang per satuan')
     if st.button('Hitung'):
-        breakeven = str(math.ceil(total_biaya_per_hari / harga_barang_per_satuan))
-        st.write('Jumlah barang yang harus anda jual tiap hari sebelum anda bisa untung adalah '+breakeven+' buah. Jika anda sulit mencapai jumlah itu, anda harus menaikkan harga barang atau menurunkan biaya usaha supaya bisa untung.')
+        breakeven = math.ceil(total_biaya_per_hari / harga_barang_per_satuan)
+        st.write('Jumlah barang yang harus anda jual tiap hari sebelum anda bisa untung adalah '+str(breakeven)+' buah. Jika anda sulit mencapai jumlah itu, anda harus menaikkan harga barang atau menurunkan biaya usaha supaya bisa untung.')
 
-'''with st.expander("Profit Margin"):
-    st.write('Breakeven (titik impas) adalah jumlah barang/jasa yang harus Anda jual sebelum Anda bisa mendapatkan keuntungan.')
-    total_biaya_per_hari = st.number_input('Total biaya per hari')
-    harga_barang_per_satuan = st.number_input('Harga barang per satuan')
-    if st.button('Hitung'):
-        breakeven = total_biaya_per_hari / harga_barang_per_satuan
-        st.write('Jumlah barang yang harus anda jual tiap hari sebelum anda bisa untung adalah'+breakeven+'buah. Jika anda sulit mencapai jumlah itu, anda harus menaikkan harga barang atau menurunkan biaya usaha supaya bisa untung.')
+with st.expander("Profit Margin"):
+    st.write('Profit margin (margin keuntungan) adalah seberapa banyak penjualan kotor anda yang dikonversi menjadi keuntungan. Dengan kata lain, ini adalah besarnya keuntungan yang anda dapat dari tiap 1 rupiah penjualan kotor anda. ')
+    penjualan_kotor  = st.number_input('Penjualan Kotor')
+    total_biaya_usaha = st.number_input('Total Biaya Usaha')
+    if st.button('Hitung '):
+        profit_margin = round((penjualan_kotor - total_biaya_usaha)/ penjualan_kotor,2)
+        st.write('Dari setiap 1 rupiah penjualan kotor anda, anda mendapatkan keuntungan sebesar '+ str(profit_margin) + ' rupiah. Dengan kata lain, jika anda berhasil mendapat total penjualan kotor sebesar Rp100.000, keuntungan anda adalah '+ profit_margin*100000 + ' rupiah. Jika anda merasa keuntungannya terlalu sedikit, anda harus meningkatkan penjualan anda atau menurunkan biaya usaha anda.')
 
-        '''
+     
